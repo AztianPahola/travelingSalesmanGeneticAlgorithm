46ec87752f051e1e34260ee3464af8a22cced7db
Chase Dawson <chase_dawson7@mymail.eku.edu>
2021-02-28 16:59:04 -0500

    Made Chromosome class. Made main method based on pseudo-code.

--- src/Default/travelingSalesman.java
+++ src/Default/travelingSalesman.java
@@ -1,5 +1,19 @@
 package Default;
 
+import java.util.*;
+
 public class travelingSalesman {
 
+	public static void main(String args()){
+		int n = 5;
+		int[][] adjecencyMatrix = {{0, 14, 4, 10, 20},
+								   {14 , 0 , 7 , 8 ,7},
+								   {4, 5, 0, 7 , 16},
+								   {11, 7, 9, 0, 2},
+								   {18, 7, 17, 4, 0}};
+		int[] resultingPath = GeneratePath(adjecencyMatrix, n);
+		for(int x : resultingPath){
+			System.out.println(x);
+		}
+	}
 }
