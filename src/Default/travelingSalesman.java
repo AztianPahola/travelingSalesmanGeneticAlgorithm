670a94eec5bf8d8d07e6adc32bee4e29bf1cec1d
Chase Dawson <chase_dawson7@mymail.eku.edu>
2021-03-01 18:15:02 -0500

    Added more comments and increased organization in main class

--- src/Default/travelingSalesman.java
+++ src/Default/travelingSalesman.java
@@ -6,58 +6,67 @@ public class travelingSalesman {
 
 	public static void main(String args[]){
 		
-		int n = 5;
+		int numCities = 5;
 		int[][] adjecencyMatrix = {{0, 14, 4, 10, 20},
 								   {14 , 0 , 7 , 8 ,7},
 								   {4, 5, 0, 7 , 16},
 								   {11, 7, 9, 0, 2},
 								   {18, 7, 17, 4, 0}};
-		int[] resultingPath = GeneratePath(adjecencyMatrix, n);
+		int[] resultingPath = generatePath(adjecencyMatrix, numCities);
 		for(int x : resultingPath){
 			System.out.println(x);
 		}
 	}
 
+	// Generate an acceptable path to take between the cities through a genetic algorithm
 	private static int[] generatePath(int[][] input, int n) {
 
 		final double MINIMUMIMPROVEMENT = 0.05; // Once the average improvement is lower than this value, the algorithm will end
 		final double MINIMUMGENERATIONS = 10;	// Minimum number of generation that will be generated
 		double improvement = 0; // Running average improvement between generations
 		int generation = 0; // Current number of generations, used to calculate improvement
-		double previousTotalStrength;
-
+		double previousTotalStrength; // Used to calculate improvement
+		int size; // Size of the population
+		chromosome[] parents; // Chromosomes selected to be crossed over to create the next generation
+		chromosome[] nextGeneration; // Chromosomes of the next generation
+		
+		// Generate generation 0 randomly
 		chromosome[] currentGeneration = generateInitialPaths(input, n);
 		
-		int size = currentGeneration.length;
+		size = currentGeneration.length;
 
 		do {
-			double totalStrength = 0; // *Add all chromosomes strengths*
+			double totalStrength = 0; // Total strength combined from all chromosomes in the generation
+			// Add all chromosomes strengths
 			for (int i = 0; i < size; i++) {
 				totalStrength += currentGeneration[i].getStrength();
 			}
 
-			double[] cumulativeStrengths = new double[size];
+			double[] cumulativeStrengths = new double[size]; /* Array of doubles which will be used as ranges to select
+															    parents for the next generation */
 			
-			// Initialize index 0 to avoid out of array index error 
+			// Initialize index 0 to avoid out of array index error in the following for loop
 			cumulativeStrengths[0] = currentGeneration[0].getStrength/totalStrength;
-			// Add the normalized strengths of each chromosome cumulatively to
-			// cumulativeStrengths array
-			for (int i = 1; i < size; i++) {
+			
+			// Add the normalized strengths of each chromosome cumulatively to the cumulativeStrengths array 
+			for (int i = 1; i < size; i++){
 				double relativeStrength = currentGeneration[i].getStrength/totalStrength;
 				cumulativeStrengths[i] = cumulativeStrengths[i-1] + relativeStrength;
 			}
-			chromosome[] parents = new chromosome[currentGeneration.length/2]; 
+			
+			parents = new chromosome[size/2]; /* Initialize parents as an empty array, 
+												 half the size of the current generation */
+			// "Randomly" select parents0
 			for (int i = 0; i < size/2; i++) {
 				parents[i] = selectRandom(currentGeneration, cumulativeStrengths, 0, size/2);
 				
 			}
-
-			chromosome[] nextGeneration = new chromosome[size];
 			
+			// Assign the nextGeneration to be the crossOver product of the parent pairs
 			nextGeneration = crossOver(parents, size, n);
 			
 
-			// Calculate change to improvement, avoiding a divide by zero error
+			// Calculate change to improvement, avoiding a divide by zero error with following if
 			if(generation > 0) {
 				improvement += (totalStrength - previousTotalStrength) / previousTotalStrength / ++generation;
 			}
@@ -66,11 +75,17 @@ public class travelingSalesman {
 		} while (generation >= MINIMUMGENERATIONS && improvement >= MINIMUMIMPROVEMENT);
 	}
 	
+	// Select a parent pseudo-randomly through a binary search for the correct range for a randomly generated double 
 	private static chromosome selectRandom(chromosome[] chromos, double[] ranges, int low, int high){
 			
-		// Generate a random double, select the correct chromosome based on that random
+		// Generate a random double, select the correct chromosome based on that random double
 		double random = Math.random();
+		
 		int mid = low+high/2;
+		
+		/* Below is a modified binary search algorithm which will use the value at mid, in addition to the values to it's left and right 
+		 * to determine if the random double is greater than mid-1 and less than mid or 
+		 * greater than or equal to mid and less than mid-1 */
 		while(low < high) {
 			
 			if(ranges[mid] < random) {
@@ -91,6 +106,7 @@ public class travelingSalesman {
 	
 	}
 	
+	// Generate generation 0 randomly
 	private static chromosome[] generateInitialPaths(int[][] input, int n) {
 		chromosome[] paths = new chromosome[2^n];
 		for (int i = 0; i < (2^n); i++) {
@@ -99,6 +115,7 @@ public class travelingSalesman {
 		return paths;
 	}
 	
+	// Interchange the indices of a given city in two parent chromosomes to produce two children
 	private static chromosome[] crossOver(chromosome[] padres, int size, int numCities) {
 		Integer randomCity;
 		
@@ -144,6 +161,7 @@ public class travelingSalesman {
 		}
 	}
 	
+	// Flip the order of a random 2 city path in a given chromosome
 	private static chromosome mutate(chromosome chromo, int numCities){
 		
 		// Select an int from 0 to numCities-1, 
